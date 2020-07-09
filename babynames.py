27a0c61b4e82905c799ae2c47bceb6d327bed00c
Tim Hindman <timmyturner45@gmail.com>
2020-07-09 16:25:57 -0400

    cootests

--- babynames.py
+++ babynames.py
@@ -7,35 +7,16 @@
 # Licensed under the Apache License, Version 2.0
 # http://www.apache.org/licenses/LICENSE-2.0
 
-"""
-Define the extract_names() function below and change main()
-to call it.
-
-For writing regex, it's nice to include a copy of the target
-text for inspiration. Here's what the HTML looks like in the
-baby.html files:
-...
-<h3 align="center">Popularity in 1990</h3>
-....
-<tr align="right"><td>1</td><td>Michael</td><td>Jessica</td>
-<tr align="right"><td>2</td><td>Christopher</td><td>Ashley</td>
-<tr align="right"><td>3</td><td>Matthew</td><td>Brittany</td>
-...
-
-Suggested milestones for incremental development:
- - Extract all the text from the file and print it
- - Find and extract the year and print it
- - Extract the names and rank numbers and print them
- - Get the names data into a dict and print it
- - Build the [year, 'name rank', ... ] list and print it
- - Fix main() to use the extracted_names list
-"""
-
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# BabyNames python coding exercise.
+# Copyright 2010 Google Inc.
+# Licensed under the Apache License, Version 2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 import sys
 import re
 import argparse
-REGEX = re.compile('<td>(\w+)</td><td>(\w+)</td><td>(\w+)</td>')
-REGEX_FILENAME = re.compile('baby(\w+).html')
+
 def extract_names(filename):
     """
     Given a single file name for babyXXXX.html, returns a
@@ -43,20 +24,28 @@ def extract_names(filename):
     the name-rank strings in alphabetical order.
     ['2006', 'Aaliyah 91', 'Aaron 57', 'Abagail 895', ...]
     """
-    names = re.findall(REGEX_FILENAME, filename)
-    names_dict = {}
-    with open (filename) as f:
-        matches = re.findall(REGEX, '\n'.join(f))
-        for match in matches:
-            if match[1] not in names_dict:
-                names_dict[match[1]] = match[0]
-            if match[2] not in names_dict:
-                names_dict[match[2]] = match[0]
-        keys_list = sorted(names_dict.keys())
-        for name in keys_list:
-            names.append(name + ' ' + names_dict[name]) 
+    with open(filename) as f:
+        text = f.read()
+    names = []
+    pattern = r'Popularity in (\d\d\d\d)'
+    year_match = re.search(pattern, text)
+    if year_match is None:
+        return None
+    year = year_match.group(1)
+    names.append(year)
+    pattern = r'<td>(\d+)</td><td>(\w+)</td><td>(\w+)</td>'
+    names_ranks = re.findall(pattern, text)
+    names_to_rank = {}
+    for rank_tuple in names_ranks:
+        rank, boy_name, girl_name = rank_tuple
+        if boy_name not in names_to_rank:
+            names_to_rank[boy_name] = rank
+        if girl_name not in names_to_rank:
+            names_to_rank[girl_name] = rank
+    sorted_names = sorted(names_to_rank.items())
+    for name, rank in sorted_names:
+        names.append(f"{name} {rank}")
     return names
-
 
 def create_parser():
     """Create a command line parser object with 2 argument definitions."""
@@ -70,40 +59,29 @@ def create_parser():
     parser.add_argument('files', help='filename(s) to parse', nargs='+')
     return parser
 
-
 def main(args):
     # Create a command line parser object with parsing rules
     parser = create_parser()
     # Run the parser to collect command line arguments into a
     # NAMESPACE called 'ns'
     ns = parser.parse_args(args)
-
     if not ns:
         parser.print_usage()
         sys.exit(1)
-
     file_list = ns.files
-
     # option flag
     create_summary = ns.summaryfile
-
     # For each filename, call `extract_names()` with that single file.
     # Format the resulting list as a vertical list (separated by newline \n).
     # Use the create_summary flag to decide whether to print the list
     # or to write the list to a summary file (e.g. `baby1990.html.summary`).
-
-    # +++your code here+++
-    if create_summary:
-        for filename in file_list:
-            names = extract_names(filename) 
-            names.append(filename)
-
-    else:
-        for filename in file_list:
-            names = extract_names(filename)
-            for name in names:
-                print(name)
-    
-
+    for filename in file_list:
+        names = extract_names(filename)
+        text = '\n'.join(names)
+        if create_summary:
+            with open(f'{filename}.summary', 'w') as f:
+                f.write(text)
+        else:
+            print(text)
 if __name__ == '__main__':
     main(sys.argv[1:])
